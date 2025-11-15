77942333b92ad5e053ac11cb18cb68d2f1ec9e84
mansingh bhagaur <mansinghbhagaur1@gmail.com>
2025-11-15 08:11:02 +0530

    theme change logic complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import { Store } from "./context/store";
 const App =()=>{
   const data = useContext(Store)
   console.log(data);
+
+  const theme = {
+    light: {
+      background: "white", color: "black", width: "100%", height: "100vh"
+    },
+    dark: {
+      background: "grey", color: "white", width: "100%", height: "100vh"
+    }
+  }
   return (
-    <>
+    <div style={{background: theme[data.ThemeMode].background, color: theme[data.ThemeMode].color, width: "100%", height: "100vh"}}>
       <h1>hello everyone aur kaise ho</h1>
       <button onClick={()=> data.setThemeMode(data.ThemeMode === "light" ? "dark" : "light")}>{data?.ThemeMode}</button>
-    </>
+    </div>
   )
 }
 

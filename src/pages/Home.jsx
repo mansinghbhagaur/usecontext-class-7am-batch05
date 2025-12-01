ee6ef41983e2c91abf9ae2173d634f6d3e310add
mansingh bhagaur <mansinghbhagaur1@gmail.com>
2025-12-01 08:01:26 +0530

    fetch topic complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,31 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+// fetch/ axois
 
 const Home = () => {
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: 'GET',
+        })
+        .then((res)=> res.json())
+        .then((result) => console.log("data: ",result) ).catch((err)=> console.log(err, "error"));
+    }, []);
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+
+            },
+            data: {
+                "title": "hello",
+                "body": "hello everyone"
+            }
+        }).then(res => console.log(res, "data res"));
+    }, []);
+
     const homename = useParams();
     return (
         <>
